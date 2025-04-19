abd0e88d84b70bd920b7047cebdcfb743eeb7912
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-04-19 18:43:41 +1000

    Add session implementation for basic API

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
+import type { InferSelectModel, InferInsertModel } from 'drizzle-orm'
+
 import { getEnvironmentVariables } from './env';
 
 const { dbHost, dbName, dbUser, dbPassword } = getEnvironmentVariables();
 
-const db = drizzle({ 
+export const db = drizzle({ 
   connection: { 
     host: dbHost,
     user: dbUser,
@@ -12,7 +15,30 @@ const db = drizzle({
     port: 5432
   }
 });
- 
+
 export const getResult = async () => {
   return await db.execute('select 1');
 }
+
+export const userTable = pgTable("app_user", {
+	id: serial("id")
+    .primaryKey(),
+  username: text("username")
+    .notNull()
+    .unique()
+});
+
+export const sessionTable = pgTable("user_session", {
+	id: text("id").primaryKey(),
+	userId: integer("user_id")
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp("expires_at", {
+		withTimezone: true,
+		mode: "date"
+	}).notNull()
+});
+
+export type User = InferSelectModel<typeof userTable>;
+export type Session = InferSelectModel<typeof sessionTable>;
+
